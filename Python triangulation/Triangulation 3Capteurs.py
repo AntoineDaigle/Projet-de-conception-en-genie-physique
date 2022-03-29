11b8a40908cdeea7ceb36f400ade25d5b1b4abab
Antoine <antoinedaigle2108rhotmail.com>
2022-03-28 22:04:31 -0400

    Ajoiut 4 eme thermistance

--- Python triangulation/Triangulation 3Capteurs.py
+++ Python triangulation/Triangulation 3Capteurs.py
@@ -1,6 +1,5 @@
-"""
-Author: Antoine Daigle
-"""
+# Tu commences par définir la position des capteurs, ensuite tu entre dans une boucle while. Dans cette boucle, tu calcul les rayons de chaque thermistances à une température hypothétique maximale ainsi que l'angle avec la position possible. Assume une décroissance exponentielle.
+
 
 # TODO Vérifier la documentation
 
@@ -13,28 +12,24 @@ import matplotlib.pyplot as plt
 
 class Triangulation:
 
-    def __init__(self, C1:tuple, C2:tuple, C3:tuple, C4:tuple, T1:float, T2:float, T3:float, T4:float) -> None:
+    def __init__(self, C1:tuple, C2:tuple, C3:tuple, T1:float, T2:float, T3:float) -> None:
         """Classe permettant de calculer par triangulation la position du faisceau ainsi que sa température. Cette classe est inspiré du code de l'équipe Cocotte-puissance.
 
         Args:
             C1 (tuple): Coordonné du premier capteur (x,y).
             C2 (tuple): Coordonné du deuxième capteur (x,y).
             C3 (tuple): Coordonné du troisième capteur (x,y).
-            C4 (tuple): Coordonné du quatrième capteur (x,y).
             T1 (float): Température du premier capteur en Celsius.
             T2 (float): Température du deuxième capteur en Celsius.
             T3 (float): Température du troisième capteur en Celsius.
-            T4 (float): Température du quatrième capteur en Celsius.
         """
 
         self.C1 = C1
         self.C2 = C2
         self.C3 = C3
-        self.C4 = C4
         self.T1 = T1
         self.T2 = T2
         self.T3 = T3
-        self.T4 = T4
 
         self.T_amb = 22     # Température ambiante supposée [Celsius]
 
@@ -68,7 +63,6 @@ class Triangulation:
             Capteur2 (tuple): Position du second capteur
             R1 (float): Rayon du premier capteur utilisé
             R2 (float): Rayon du second capteur utilisé
-
 
         Returns:
             tuple: Coordonnée de la position du faisceau
@@ -123,7 +117,7 @@ class Triangulation:
 
 
     def Rayon_max(self) -> float:
-        """Méthode qui calcule le rayon maximal maximum possible. L'utilité de cette méthode est de faire une borne maximale. 
+        """Méthode qui calcule le rayon maximal maximum possible.
 
         Returns:
             float: Rayon maximal possible
@@ -137,93 +131,66 @@ class Triangulation:
 
 
     def Itération_tentative(self):
-        """Méthode qui calcul de manière itérative la position du faisceau. C'est cette méthode qui est la plus critique. Je sais pas trop comment l'arranger. 
-
-        Méthode qui continent les itérations.
+        """Méthode qui calcul de manière itérative la position du faisceau. C'est cette méthode qui est la plus critique. Je sais pas trop comment l'arranger
         """
 
 
+        T_max = 300
+
+        DiffR = 1
+
+        R_max = self.Rayon_max()
+
+        SAFE=1
 
 
-        def Calcul_itératif(Therm, Cap):
+        while abs(DiffR) > 0.001:
 
+            SAFE +=1
 
-            T_max = 300
-            DiffR = 1
-            R_max = self.Rayon_max()
-
-
-            SAFE=1
-            while abs(DiffR) > 0.001:
-                SAFE +=1
-
-                R1 = self.Distance_rayon_a_faisceau(self.T1, T_max)            
-                R2 = self.Distance_rayon_a_faisceau(self.T2, T_max)
-                R3 = self.Distance_rayon_a_faisceau(Therm, T_max)
+            R1 = self.Distance_rayon_a_faisceau(self.T1, T_max)            
+            R2 = self.Distance_rayon_a_faisceau(self.T2, T_max)
+            R3 = self.Distance_rayon_a_faisceau(self.T3, T_max)
 
 
 
-                R_actuel = R1 + R2 + R3
+            R_actuel = R1 + R2 + R3
 
-                DiffR = R_actuel - R_max
+            DiffR = R_actuel - R_max
 
-                if R_actuel > R_max:
-                    T_max = T_max*0.95
+            if R_actuel > R_max:
+                T_max = T_max*0.95
 
-                elif SAFE == 1000:
-                    print("SAFE")
-                    break
+            elif SAFE == 1000:
+                print("SAFE")
+                break
 
-                else:
-                    T_max = T_max * 1.05
-
-            PositionFinale = self.Position_faisceau(self.C1, self.C2, R1, R2)
-
-            
-
-            print("­\n- - RESULTS - -\n")
-            print("\tRayon 1: {}\n\tRayon 2: {}\n\tRayon 3: {}\n".format(R1, R2, R3))
-            if Cap == 3:
-                print("\tPosition: ({}, {})\n\tTempérature: {} C\n".format(round(PositionFinale[0], 4), round(PositionFinale[1], 4), round(T_max + self.T_amb, 2)))
             else:
-                print("\tPosition: ({}, {})\n\tTempérature: {} C\n".format(round(PositionFinale[0], 4), round(-1*PositionFinale[1], 4), round(T_max + self.T_amb, 2)))
+                T_max = T_max * 1.05
 
-            return self.C1, self.C2, self.C3, self.C4, R1, R2, R3, PositionFinale, Cap
-            
+        PositionFinale = self.Position_faisceau(self.C1, self.C2, R1, R2)
 
-            
+        print("­\n- - RESULTS - -\n")
+        print("\tRayon 1: {}\n\tRayon 2: {}\n\tRayon 3: {}\n".format(R1, R2, R3))
+        print("\tPosition: ({}, {})\n\tTempérature: {} C\n".format(round(PositionFinale[0], 4), round(PositionFinale[1], 4), round(T_max + self.T_amb, 2)))
 
-        if self.T3 > self.T4:
-            return Calcul_itératif(self.T3, 3)
-
-        else:
-            return Calcul_itératif(self.T4, 4)
-            
+        return self.C1, self.C2, self.C3, R1, R2, R3, PositionFinale
 
 
 
 
-
-def PLOT(C1, C2, C3, C4, R1, R2, R3, Pos, Cap):
-
-    if Cap == 3:
-        Therm = C3
-        PointY = Pos[1]
-    else:
-        Therm = C4
-        PointY = -1*Pos[1]
-
+def PLOT(C1, C2, C3, R1, R2, R3, Pos):
     figure, axes = plt.figure(), plt.gca()
 
     Cir1 = plt.Circle(C1, R1, color="b", fill=False)
     Cir2 = plt.Circle(C2, R2, color="r", fill=False)
-    Cir3 = plt.Circle(Therm, R3, color="g", fill=False)
+    Cir3 = plt.Circle(C3, R3, color="g", fill=False)
 
     axes.add_patch(Cir1)
     axes.add_patch(Cir2)
     axes.add_patch(Cir3)
-    plt.scatter([C1[0], C2[0], C3[0], C4[0]], [C1[1], C2[1], C3[1], C4[1]], color="black")
-    plt.scatter([Pos[0]], [PointY], color="fuchsia", edgecolors= 'k', s= 150)
+    plt.scatter([C1[0], C2[0], C3[0]], [C1[1], C2[1], C3[1]], color="black")
+    plt.scatter([Pos[0]], [Pos[1]], color="fuchsia", edgecolors= 'k', s= 150)
     plt.axhline(0, c="black")
     plt.axvline(0, c="black")
     plt.axis('scaled')
@@ -233,9 +200,7 @@ def PLOT(C1, C2, C3, C4, R1, R2, R3, Pos, Cap):
 
 
 if __name__ == "__main__":
-    PLOT(*Triangulation((0,0), (6,0), (3,9), (3, -9), 46, 35, 30, 90).Itération_tentative())
-    # PLOT(*Triangulation((0,0), (6,0), (3,9), (3, -9), 46, 60, 30, 90).Itération_tentative())
-    # PLOT(*Triangulation((0,0), (6,0), (3,9), (3, -9), 46, 35, 90, 30).Itération_tentative())
-    # PLOT(*Triangulation((0,0), (6,0), (3,9), (3, -9), 46, 35, 60, 30).Itération_tentative())
-    # PLOT(*Triangulation((0,0), (6,0), (3,9), (3, -9), 26, 30, 22, 27).Itération_tentative())
-
+    PLOT(*Triangulation((0,0), (6,0), (3,9), 46, 35, 82).Itération_tentative())
+    # PLOT(*Triangulation((0,0), (6,0), (3,9), 30, 40, 70).Itération_tentative())
+    # PLOT(*Triangulation((0,0), (6,0), (3,9), 45 ,50, 35).Itération_tentative())
+    # PLOT(*Triangulation(np.array([0, 0]), np.array([5.1961524*2, 0]), np.array([5.1961524, 9.0]), 45.796, 23.575, 81.817).Itération_tentative() )  # Pas valide, car premier capteur pas à (0,0)

dc2e682ec02b03e7402d36dfbd56acd8fb93fff3
Antoine <antoinedaigle2108rhotmail.com>
2022-04-24 08:53:12 -0400

    ajout rap

--- Python triangulation/Simulation Labview.py
+++ Python triangulation/Simulation Labview.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 
 
-Therms = ReadDATA(r"Python triangulation\Data de tests\Test MAPA 21 avril\Mesure 3.lvm")
+Therms = ReadDATA(r"Python triangulation\Data de tests\Échec monumental\ExamenChaleur.lvm")
 
 
 
@@ -32,33 +32,60 @@ for i in Therms:
     T_max.append(Raw[9])
 
 
+d = {"PosX": PosX, "PosY": PosY, "T_max": T_max}
+df = pd.DataFrame(data=d)
+
+def Puissance(T_max, l):
+    a = (-1e-12)*l**4 + (8e-9)*l**3 - (3e-5)*l**2 + 0.0423*l - 12.887
+    return (T_max-21.9)/a
+
+def Rapport():
+
+    NormT = df["T_max"]/max(df["T_max"])
+
+    Power = []
+
+    for i in df["T_max"]:
+        Power.append(Puissance(i, 793))
+
+    df["Puissance"] = Power
+
+    NormP = df["Puissance"]/max(df["Puissance"])
+    # print(NormT)
+
+    xrange = range(len(NormP))
+
+    plot(xrange, NormP, label="Puissance normalisé")
+    plot(xrange, NormT, label="Température normalisé")
+    xlabel("Mesure")
+    legend()
+    savefig(r"Python triangulation\Data de tests\FIGURE\ExamenChaleur RAPPORT.png", dpi=600)
+    show()
 
 
-    def CreateDF(l):
-
-        d = {"PosX": PosX, "PosY": PosY, "T_max": T_max}
-        df = pd.DataFrame(data=d)
-
-        def Puissance(T_max, l):
-            a = (-1e-12)*l**4 + (8e-9)*l**3 - (3e-5)*l**2 + 0.0423*l - 12.887
-            return (T_max-21.9)/a
+Rapport()
 
 
-        fig, ax = subplots(4)
-
-        x = range(len(df["T_max"]))
-
-        histplot(df["PosX"], ax=ax[0])
-        histplot(df["PosY"], ax=ax[1])
-        ax[2].plot(x, df["T_max"])
-        ax[2].set(ylabel=r"Température [$^\circ$C]", xlabel="Mesure")
-
-        ax[3].plot(x, Puissance(df["T_max"], l))
-        ax[3].set(ylabel=r"Puissance [W]", xlabel="Mesure")
-
-        tight_layout()
-        savefig(r"Python triangulation\Data de tests\FIGURE\Mesure 3.png", dpi=600)
-        show()
 
 
-CreateDF(793)
+
+
+def CreateDF(l):
+    fig, ax = subplots(4)
+
+    x = range(len(df["T_max"]))
+
+    histplot(df["PosX"], ax=ax[0])
+    histplot(df["PosY"], ax=ax[1])
+    ax[2].plot(x, df["T_max"])
+    ax[2].set(ylabel=r"Température [$^\circ$C]", xlabel="Mesure")
+
+    ax[3].plot(x, Puissance(df["T_max"], l))
+    ax[3].set(ylabel=r"Puissance [W]", xlabel="Mesure")
+
+    tight_layout()
+    # savefig(r"Python triangulation\Data de tests\FIGURE\Mesure 3.png", dpi=600)
+    show()
+
+
+# CreateDF(793)
